4f92cec0519b42a3e9a44b82dfaf0cb6801ef0c6
vukomanv <vladimir.vukoman.v@gmail.com>
2023-05-10 11:13:46 +0200

    Added appointment fields from the model

--- import-catalog-acc.sql
+++ import-catalog-acc.sql
@@ -10,13 +10,21 @@ CREATE TABLE address (
 );
 
 CREATE TABLE accommodation (
-    id bigint not null auto_increment,
-    address_id bigint,
-    name varchar(255),
-    minimum_guests int, 
-    maximum_guests int,
-    facilities varchar(255),
-    PRIMARY KEY(id),
+    id bigint NOT NULL AUTO_INCREMENT,
+    address_id bigint DEFAULT NULL,
+    name varchar(255) DEFAULT NULL,
+    minimum_guests int DEFAULT NULL,
+    maximum_guests int DEFAULT NULL,
+    facilities varchar(255) DEFAULT NULL,
+    accommodation_grade_id bigint DEFAULT NULL,
+    available_beds int NOT NULL,
+    is_auto bit(1) DEFAULT NULL,
+    location varchar(255) NOT NULL,
+    max_guests int NOT NULL,
+    min_guests int NOT NULL,
+    photo varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    KEY `address_id` (address_id),
     CONSTRAINT FOREIGN KEY (address_id) REFERENCES address(id)
 );
 
