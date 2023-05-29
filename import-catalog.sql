6cb0a05f1c7fe8e3682b06d6b701df9ed04c8b6e
spiricnra2052019 <106819752+spiricnra2052019@users.noreply.github.com>
2023-05-29 14:23:48 +0200

    Rate added

--- import-catalog.sql
+++ import-catalog.sql
@@ -18,7 +18,7 @@ INSERT INTO address (country, city, street, number, longitude, latitude) VALUES 
 INSERT INTO address (country, city, street, number, longitude, latitude) VALUES ('Indonesia', 'Denpasar', 'Milana Savica', '12', 19.796963, 45.240372);
 INSERT INTO address (country, city, street, number, longitude, latitude) VALUES ('Thailand', 'Bangkok', 'Milana Savica', '12', 19.796963, 45.240372);
 
-CREATE TABLE users (
+CREATE TABLE user (
     id bigint not null auto_increment,
     email varchar(255),
     username varchar(255),
@@ -34,5 +34,17 @@ CREATE TABLE users (
     PRIMARY KEY(id)
 );
 
-INSERT INTO user (email, username, first_name, last_name, phone_number, role, location, avgGrade, isHighlighted) 
+INSERT INTO user (email, username, first_name, last_name, phone_number, role, location, avg_grade, is_highlighted) 
           VALUES ('kalina@gmail.com', 'kalina', 'Nikola', 'Kalinic', '0658897554', 'HOST', 'bla bla', 0, FALSE);
+
+
+CREATE TABLE rate (
+	id bigint not null auto_increment,
+	host_id bigint not null,
+	guest_id bigint not null,
+	rate_value int not null,
+	rate_date datetime not null,
+	PRIMARY KEY(id),
+	CONSTRAINT  FOREIGN KEY(guest_id) REFERENCES user(id),
+    	CONSTRAINT  FOREIGN KEY(host_id) REFERENCES user(id)
+);
